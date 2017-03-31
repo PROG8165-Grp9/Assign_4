eb44fc43ad59574fd47b88d53319a0aeaa9fdc97
Lea Monica Belo <lea.belo@delasalle.ph>
2017-03-31 15:33:04 -0400

    Upload latest

--- Django_Assign/myTrans/admin.py
+++ Django_Assign/myTrans/admin.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Users
+from .models import Transaction
 
 class UserDetails(admin.ModelAdmin):
    list_display = ['Username','Email']
 
+class TransDetails(admin.ModelAdmin):
+   list_display = ['id','Trans_Desc']
+
 admin.site.register(Users,UserDetails)
+admin.site.register(Transaction,TransDetails)
