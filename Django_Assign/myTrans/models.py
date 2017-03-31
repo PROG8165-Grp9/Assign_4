73eeffd6a7fc781ac32c13e087d3cf9cae49faaf
Lea Monica Belo <lea.belo@delasalle.ph>
2017-03-31 14:04:16 -0400

    Modified database files

--- Django_Assign/myTrans/models.py
+++ Django_Assign/myTrans/models.py
@@ -5,3 +5,10 @@ class Users(models.Model):
     Username = models.CharField(max_length=200)
     Password = models.CharField(max_length=16)
     Email = models.CharField(max_length=200)
+
+class Transaction(models.Model):
+    Trans_Desc = models.TextField()
+    Trans_Date = models.DateTimeField()
+    Trans_Type = models.CharField(max_length=50)
+    Trans_Loc = models.CharField(max_length=150)
+    Trans_Amnt = models.DecimalField
